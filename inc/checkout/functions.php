175ac470434cc79940cb4188dc7b862a9336b759
Abid Omar <contact@omarabid.com>
2014-10-19 22:52:24 +0100

    Checkout functions cleanup

--- inc/checkout/functions.php
+++ inc/checkout/functions.php
@@ -21,15 +21,15 @@ if (!defined('ABSPATH')) {
  * @return bool
  */
 function wp_adpress_register_ad( $ad_details, $user_info ) {
-    if ( !is_array( $ad_details ) || !is_array( $user_info ) ) {
-        return;
-    }
+	if ( !is_array( $ad_details ) || !is_array( $user_info ) ) {
+		return;
+	}
 
-    $campaign = new wp_adpress_campaign( $ad_details['cid'] );
+	$campaign = new wp_adpress_campaign( $ad_details['cid'] );
 
-    $ad_id = $campaign->register_ad( $ad_details['post'] );
+	$ad_id = $campaign->register_ad( $ad_details['post'] );
 
-    return $ad_id;
+	return $ad_id;
 }
 
 /**
@@ -49,10 +49,10 @@ function wp_adpress_get_payment_user_info( $payment_id ) {
 	 */
 	$user_info = array();
 
-    $user_info['user_id'] = get_post_meta( $payment_id, 'wpad_payment_user_id', true );
-    $user_info['user_email'] = get_post_meta( $payment_id, 'wpad_payment_user_email', true );
-    $user_info['user_info'] = get_post_meta( $payment_id, 'wpad_payment_user_info', true );
-    
+	$user_info['user_id'] = get_post_meta( $payment_id, 'wpad_payment_user_id', true );
+	$user_info['user_email'] = get_post_meta( $payment_id, 'wpad_payment_user_email', true );
+	$user_info['user_info'] = get_post_meta( $payment_id, 'wpad_payment_user_info', true );
+
 	return $user_info;
 }
 
@@ -75,7 +75,7 @@ function wp_adpress_get_payment_ad_details( $payment_id ) {
 	 */
 	$ad_details = array();
 
-    $ad_details = get_post_meta( $payment_id, 'wpad_payment_ad', true );
+	$ad_details = get_post_meta( $payment_id, 'wpad_payment_ad', true );
 
 	return $ad_details;
 }
@@ -88,7 +88,14 @@ function wp_adpress_get_payment_ad_details( $payment_id ) {
  * @return mixed Full URL to the success page, if present | null if it doesn't exist
  */
 function wp_adpress_get_success_page_uri( $query_args = array() ) {
+	$uri = admin_url( 'admin.php?page=adpress-checkout-success' );
 
+	if ( !empty( $query_args ) ) {
+		$query_args = wp_parse_args( $query_args );
+		$uri = add_query_arg( $query_args, $uri );
+	}
+
+	return apply_filters( 'wp_adpress_get_success_page_uri', $uri );	
 }
 
 /**
@@ -100,9 +107,13 @@ function wp_adpress_get_success_page_uri( $query_args = array() ) {
  * @access      public
  * @since       1.0.0
  * @return      void
-*/
+ */
 function wp_adpress_send_to_success_page( $query_args = array() ) {
+	$redirect = wp_adpress_get_success_page_uri( $query_args );
 
+	wp_redirect( apply_filters( 'wp_adpress_send_to_success_page', $redirect, $query_args ) );
+
+	wp_adpress_die();
 }
 
 /**
@@ -113,7 +124,14 @@ function wp_adpress_send_to_success_page( $query_args = array() ) {
  * @return mixed Full URL to the failure page, if present | null if it doesn't exist
  */
 function wp_adpress_get_failure_page_uri( $query_args = array() ) {
+	$uri = admin_url( 'admin.php?page=adpress-checkout-failure' );
 
+	if ( !empty( $query_args ) ) {
+		$query_args = wp_parse_args( $query_args );
+		$uri = add_query_arg( $query_args, $uri );
+	}
+
+	return apply_filters( 'wp_adpress_get_failure_page_uri', $uri );
 }
 
 /**
@@ -125,9 +143,13 @@ function wp_adpress_get_failure_page_uri( $query_args = array() ) {
  * @access      public
  * @since       1.0.0
  * @return      void
-*/
+ */
 function wp_adpress_send_to_failure_page( $query_args = array() ) {
+	$redirect = wp_adpress_get_failure_page_uri( $query_args );
 
+	wp_redirect( apply_filters( 'wp_adpress_send_to_failure_page', $redirect, $args ) );
+
+	wp_adpress_die();
 }
 
 /**
@@ -138,7 +160,14 @@ function wp_adpress_send_to_failure_page( $query_args = array() ) {
  * @return mixed Full URL to the checkout page, if present | null if it doesn't exist
  */
 function wp_adpress_get_checkout_page_uri( $query_args = array() ) {
+	$uri = admin_url( 'admin.php?page=adpress-client' );
 
+	if ( !empty( $query_args ) ) {
+		$query_args = wp_parse_args( $query_args );
+		$uri = add_query_arg( $query_args, $uri );
+	}
+
+	return apply_filters( 'wp_adpress_get_checkout_page_uri', $uri );
 }
 
 /**
@@ -150,7 +179,11 @@ function wp_adpress_get_checkout_page_uri( $query_args = array() ) {
  * @access      public
  * @since       1.0.0
  * @return      void
-*/
+ */
 function wp_adpress_send_to_checkout_page( $query_args = array() ) {
+	$redirect = wp_adpress_get_checkout_page_uri( $query_args );
 
+	wp_redirect( apply_filters( 'wp_adpress_send_to_checkout_page', $redirect, $args ) );
+
+	wp_adpress_die();
 }
