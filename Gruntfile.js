9ee0c4a4786b64f46b7a434e8772d4f2cc1d9193
Abid Omar <contact@omarabid.com>
2015-03-19 21:50:32 +0100

    feat(grunt): Improved Grunt process

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,74 @@
 module.exports = function( grunt ) {
 	'use strict';
 
+	// Load multiple grunt tasks using globbing patterns
+	require('load-grunt-tasks')(grunt);
+
 	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		// project directories
+		dirs: {
+			lang: 'i18n/languages',
+		},
+
+	// Check that textdomain is set
+		checktextdomain: {
+			options:{
+				text_domain: 'wp-adpress',
+				create_report_file: false,
+				keywords: [
+					'__:1,2d',
+					'_e:1,2d',
+					'_x:1,2c,3d',
+					'esc_html__:1,2d',
+					'esc_html_e:1,2d',
+					'esc_html_x:1,2c,3d',
+					'esc_attr__:1,2d',
+					'esc_attr_e:1,2d',
+					'esc_attr_x:1,2c,3d',
+					'_ex:1,2c,3d',
+					'_n:1,2,3,4d',
+					'_nx:1,2,4c,5d',
+					'_n_noop:1,2,3d',
+					'_nx_noop:1,2,3c,4d',
+					' __ngettext:1,2,3d',
+					'__ngettext_noop:1,2,3d',
+					'_c:1,2d',
+					'_nc:1,2,4c,5d'
+				]
+			},
+			files: {
+				src: [
+					'**/*.php', // Include all files
+					'!node_modules/**', // Exclude node_modules/
+					'!tests/**', // Exclude unit tests/
+					'!bin/**', // Exclude Bin/
+					'!i18n/**', // Exclude i18n/
+					'!build/.*'// Exclude build/
+				],
+				expand: true
+			}
+		},
+
+		// Generate the mo files
+		potomo: {
+			dist: {
+				options: {
+					poDel: false 
+				},
+				files: [{
+					expand: true,
+					cwd: '<%= dirs.lang %>',
+					src: ['*.po'],
+					dest: '<%= dirs.lang %>',
+					ext: '.mo',
+					nonull: true
+				}]
+			}
+		},
+
+		// Generate POT file
 		makepot: {
 			target: {
 				options: {
@@ -10,13 +77,70 @@ module.exports = function( grunt ) {
 					type: 'wp-plugin',                // Type of project (wp-plugin or wp-theme).
 				}
 			}
+		},
+
+		// Clean up build directory
+		clean: {
+			main: ['build']
+		},
+
+		// Copy the plugin files into the build directory
+		copy: {
+			main: {
+				src:  [
+					'**',
+					'!node_modules/**',
+					'!bin/**',
+					'!build/**',
+					'!tests/**',
+					'!help/**',
+					'!.git/**',
+					'!.idea/**',
+					'!Gruntfile.js',
+					'!package.json',
+					'!phpunit.xml',
+					'!.scrutinizer.yml',
+					'!.travis.yml',
+					'!.gitignore',
+					'!.gitmodules',
+					'!README.md',
+					'!README.txt',
+				],
+				dest: 'build/<%= pkg.name %>/plugin/'
+			},
+			help: {
+				src: [ 'help/**' ],
+				dest: 'build/<%= pkg.name %>/'
+			}
+		},
+
+		//Compress build directory into <name>.zip 
+		compress: {
+			plugin: {
+				options: {
+					mode: 'zip',
+					archive: './build/<%= pkg.name %>/<%= pkg.name %>-plugin.zip'
+				},
+				expand: true,
+				cwd: 'build/<%= pkg.name %>/plugin/',
+				src: ['**/*'],
+				dest: '<% pkg.name %>/plugin/'
+			},
+			build: {
+				options: {
+					mode: 'zip',
+					archive: './build/<%= pkg.name %>.zip'
+				},
+				expand: true,
+				cwd: 'build/<%= pkg.name %>/',
+				src: ['**/*', '!plugin/**'],
+				dest: '<%= pkg.name %>/'
+			}
 		}
 	});	
 
-	// Load NPM tasks to be used here	
-	grunt.loadNpmTasks( 'grunt-wp-i18n' );
-
 	// Register tasks
-	grunt.registerTask('default', ['makepot']);
+	grunt.registerTask( 'default', ['makepot', 'potomo', 'clean', 'copy', 'compress'] );
 
+	grunt.registerTask( 'report', ['checktextdomain'] );
 };
