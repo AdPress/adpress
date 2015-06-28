93e97ec1fa5f1fac9aeaad6b183b2a58e25385f0
Abid Omar <contact@omarabid.com>
2015-06-28 18:28:59 +0100

    Changed the Plugin name to avoid collision with a similar plugin in the wordpress.org repo

--- wp-adpress.php
+++ wp-adpress.php
@@ -8,7 +8,7 @@
  * @package Main
  */
 /*
-  Plugin Name: AdPress
+  Plugin Name: WP AdPress
   Plugin URI: http://wpadpress.com
   Description: AdPress is a fully featured Ads Manager for WordPress with client management, PayPal Integration, analytics and Multi-site support.
   Author: Abid Omar
