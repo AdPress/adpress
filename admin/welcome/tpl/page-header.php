f8cc5f333bb9641d657b0313925b077fa4386c4c
Abid Omar <contact@omarabid.com>
2015-07-01 21:54:06 +0100

    New AdPress Logo

--- admin/welcome/tpl/page-header.php
+++ admin/welcome/tpl/page-header.php
@@ -1,3 +1,5 @@
 		<div class="wrap about-wrap">
+			<img class="start-logo" src="<?php echo ADPRESS_URLPATH . 'admin/welcome/assets/img/logo.png'; ?>" />
 			<h1><?php printf( __( 'Welcome to AdPress %s', 'wp-adpress' ), WPAD()->version ); ?></h1>
+			<div class="clear"></div>
 			<div class="about-text"><?php printf( __( 'Thank you for choosing AdPress! We hope AdPress will change your experience on managing Ads for WordPress', 'wp-adpress' ), WPAD()->version ); ?></div>
