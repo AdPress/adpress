151d11a56e2bcdd669744c90eede8e003cdf4c43
Abid Omar <contact@omarabid.com>
2015-07-02 00:49:55 +0100

    Updated pointers file

--- admin/pointers.php
+++ admin/pointers.php
@@ -2,13 +2,11 @@
 
 //add_action('admin_enqueue_scripts', 'myHelpPointers');
 function myHelpPointers() {
-	require_once( 'class.wp-help-pointers.php' );
 global $current_screen;
-//var_dump( $current_screen );
 	//First we define our pointers 
 	$pointers = array(
 		array(
-			'id' => 'xyz12',   // unique id for this pointer
+			'id' => 'xyz1f2',   // unique id for this pointer
 			'screen' => 'dashboard_page_wpadpress-new', // this is the page hook we want our pointer to show on
 			'target' => '#wpadminbar', // the css selector for the pointer to be tied to, best to use ID's
 			'title' => 'My ToolTip',
@@ -21,5 +19,5 @@ global $current_screen;
 		// more as needed
 	);
 	//Now we instantiate the class and pass our pointer array to the constructor 
-	$myPointers = new WP_Help_Pointer($pointers);
+	$myPointers = new \wpplex\WP_Pointer\WP_Pointer($pointers);
 }
