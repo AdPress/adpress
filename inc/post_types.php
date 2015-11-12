3e036ab01f54599e5a3c724da623651bd592dc13
Abid Omar <contact@omarabid.com>
2015-11-12 17:41:43 +0100

    Campaigns and Ads post type registration functions

--- inc/post_types.php
+++ inc/post_types.php
@@ -120,3 +120,83 @@ function wp_adpress_adshistory_post_type() {
 }
 
 add_action( 'init', 'wp_adpress_adshistory_post_type' );
+
+/**
+ * Campaigns post type
+ *
+ */
+function wp_adpress_campaigns_post_type() {
+
+	// Post Type labels
+	$campaigns_labels = array(
+		'name' 				=> _x( 'Campaigns', 'post type general name', 'wp-adpress' ),
+		'singular_name' 	=> _x( 'Campaign', 'post type singular name', 'wp-adpress' ),
+		'add_new' 			=> __( 'Add New', 'wp-adpress' ),
+		'add_new_item' 		=> __( 'Add New Campaign', 'wp-adpress' ),
+		'edit_item' 		=> __( 'Edit Campaign', 'wp-adpress' ),
+		'new_item' 			=> __( 'New Campaign', 'wp-adpress' ),
+		'all_items' 		=> __( 'All Campaigns', 'wp-adpress' ),
+		'view_item' 		=> __( 'View Campaign', 'wp-adpress' ),
+		'search_items' 		=> __( 'Search Campaigns', 'wp-adpress' ),
+		'not_found' 		=> __( 'No Campaigns found', 'wp-adpress' ),
+		'not_found_in_trash'=> __( 'No Campaigns found in Trash', 'wp-adpress' ),
+		'parent_item_colon' => '',
+		'menu_name' 		=> __( 'AdPress Campaigns', 'wp-adpress' ),
+	);
+
+	// Post Type args
+	$campaigns_args = array(
+		'labels' 			=> apply_filters( 'wp_adpress_payment_labels', $campaigns_labels ),
+		'public' 			=> defined( 'WP_DEBUG' ) && WP_DEBUG,
+		'query_var' 		=> false,
+		'rewrite' 			=> false,
+		'map_meta_cap'      => true,
+		'supports' 			=> array( 'title' ),
+		'can_export'		=> true,
+	);
+
+	// Register the post type
+	register_post_type( 'wp_adpress_campaigns', $campaigns_args );
+}
+
+add_action('init', 'wp_adpress_campaigns_post_type');
+
+/**
+ * Ads post type
+ *
+ */
+function wp_adpress_ads_post_type() {
+
+	// Post Type labels
+	$ads_labels = array(
+		'name' 				=> _x( 'Ads', 'post type general name', 'wp-adpress' ),
+		'singular_name' 	=> _x( 'Ads', 'post type singular name', 'wp-adpress' ),
+		'add_new' 			=> __( 'Add New', 'wp-adpress' ),
+		'add_new_item' 		=> __( 'Add New Ad', 'wp-adpress' ),
+		'edit_item' 		=> __( 'Edit Ad', 'wp-adpress' ),
+		'new_item' 			=> __( 'New Ad', 'wp-adpress' ),
+		'all_items' 		=> __( 'All Ads', 'wp-adpress' ),
+		'view_item' 		=> __( 'View Ad', 'wp-adpress' ),
+		'search_items' 		=> __( 'Search Ads', 'wp-adpress' ),
+		'not_found' 		=> __( 'No Ads found', 'wp-adpress' ),
+		'not_found_in_trash'=> __( 'No Ads found in Trash', 'wp-adpress' ),
+		'parent_item_colon' => '',
+		'menu_name' 		=> __( 'AdPress Ads', 'wp-adpress' ),
+	);
+
+	// Post Type args
+	$ads_args = array(
+		'labels' 			=> apply_filters( 'wp_adpress_payment_labels', $ads_labels ),
+		'public' 			=> defined( 'WP_DEBUG' ) && WP_DEBUG,
+		'query_var' 		=> false,
+		'rewrite' 			=> false,
+		'map_meta_cap'      => true,
+		'supports' 			=> array( 'title' ),
+		'can_export'		=> true,
+	);
+
+	// Register the post type
+	register_post_type( 'wp_adpress_ads', $ads_args );
+}
+
+add_action('init', 'wp_adpress_ads_post_type');
