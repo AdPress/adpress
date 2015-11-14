199f21ecde70a5a8f6cbb65588b7b3455e72c08a
Abid Omar <contact@omarabid.com>
2015-11-14 16:38:49 +0100

    Campaign functions skeleton

--- inc/campaigns/functions.php
+++ inc/campaigns/functions.php
@@ -11,3 +11,63 @@
 if (!defined('ABSPATH')) {
 	die('-1');
 }
+
+/**
+ * Creates a new Campaign 
+ * 
+ * @since 1.2.0 
+ */
+function wp_adpress_insert_new_campaign ( $settings, $ad_definition ) {	
+}
+
+/**
+ * Get Campaign by Id 
+ * 
+ * @since 1.2.0 
+ */
+function wp_adpress_get_campaign( $id ) {
+}
+
+/**
+ * Update Campaign by Id 
+ * 
+ * @since 1.2.0 
+ */
+function wp_adpress_update_campaign( $id, $settings, $ad_definition ) {
+}
+
+/**
+ * Activate Campaign by Id 
+ * 
+ * @since 1.2.0 
+ */
+function wp_adpress_activate_campaign( $id ) {
+
+}
+
+/**
+ * Deactivate Campaign by Id 
+ * 
+ * @since 1.2.0 
+ */
+function wp_adpress_deactivate_campaign( $id ) {
+
+}
+
+/**
+ * Get Campaign Status (active/inactive) by Id 
+ * 
+ * @since 1.2.0 
+ */
+function wp_adpress_is_campaign_active( $id ) {
+
+}
+
+/**
+ * Delete Campaign by Id 
+ * 
+ * @since 1.2.0 
+ */
+function wp_adpress_delete_campaign( $id ) {
+
+}
