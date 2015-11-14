1d9695b8ec85a4cb810793b9e9a1e6721427d877
Abid Omar <contact@omarabid.com>
2015-11-14 11:52:57 +0100

    Campaigns actions and functions files

---
+++ inc/campaigns/actions.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Campaigns Related Actions 
+ *
+ * @package     Includes
+ * @subpackage  Campaigns 
+ * @since       1.2.0
+ */
+
+// Don't load directly
+if (!defined('ABSPATH')) {
+	die('-1');
+}
